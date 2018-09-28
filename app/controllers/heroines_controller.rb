b282903fc2d24456b0f2d8a56b89923bfbacd3df
gwatson86 <gwatson86@gmail.com>
2018-09-28 11:19:07 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    if params[:search]
+      @heroines = Heroine.where("power LIKE ?", "%#{params[:power]}%")
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.save
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:super_name, :name, :power_id)
   end
 end
