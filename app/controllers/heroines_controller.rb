532fe05b4036ef9a98dbcee2a23ef597db892a9d
V10LET <violet.wmoon@gmail.com>
2018-09-28 11:20:10 -0500

    mod 2 challenge superheroine masterpiece

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,52 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    @powers = if params[:power]
+    Power.where('name LIKE ?', "%#{params[:power]}%")
+ else
+    Power.all
+ end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    heroine = Heroine.new(h_params)
+    if heroine.valid?
+      heroine.save
+      redirect_to heroine_path(heroine)
+    else
+      flash[:errors] = heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def update
+    if @heroine = Heroine.update(params[:id], h_params)
+      redirect_to heroine_path(@heroine)
+    else
+     flash[:errors] = heroine.errors.full_messages
+     redirect_to new_heroine_path
+    end
+  end
+
+  def destroy
+    Heroine.find(params[:id]).destroy
+    redirect_to heroines_url
+  end
+
+private
+  def h_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
